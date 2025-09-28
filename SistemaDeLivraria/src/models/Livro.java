3d19f8a98a0d5100e3ad03f13bec9f6fe0468fbb
Arth1Droid <barbosarthur7@gmail.com>
2025-09-27 22:30:55 -0300

    Adiciona linhas classe Livro e Status

--- SistemaDeLivraria/src/models/Livro.java
+++ SistemaDeLivraria/src/models/Livro.java
@@ -1,5 +1,74 @@
 package models;
 
-public class Livro {
+import java.time.LocalDateTime;
 
+public class Livro {
+	private int id;
+	private String titulo;
+	private String autor;
+	private Status status;
+	private LocalDateTime dataCadastro;
+	private LocalDateTime dataAtualizacao;
+	
+	public Livro(int id, String titulo, String autor, LocalDateTime dataCadastro, LocalDateTime dataAtualizacao) {
+		this.id = id;
+		this.titulo = titulo;
+		this.autor = autor;
+		this.status = Status.DISPONIVEL;
+		this.dataCadastro = dataCadastro;
+		this.dataAtualizacao = dataAtualizacao;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getTitulo() {
+		return titulo;
+	}
+
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+
+	public String getAutor() {
+		return autor;
+	}
+
+	public void setAutor(String autor) {
+		this.autor = autor;
+	}
+
+	public Status getStatus() {
+		return status;
+	}
+
+	public void setStatus(Status status) {
+		this.status = status;
+	}
+
+	public LocalDateTime getDataCadastro() {
+		return dataCadastro;
+	}
+
+	public void setDataCadastro(LocalDateTime dataCadastro) {
+		this.dataCadastro = dataCadastro;
+	}
+
+	public LocalDateTime getDataAtualizacao() {
+		return dataAtualizacao;
+	}
+
+	public void setDataAtualizacao(LocalDateTime dataAtualizacao) {
+		this.dataAtualizacao = dataAtualizacao;
+	}
+	
+	public String toString() {
+		return "Livro: " + titulo + "\nAutor: " + autor + " \nStatus: " + status + "\nData de cadastro: " + dataCadastro;
+	}
+	
 }
--- SistemaDeLivraria/src/models/Status.java
+++ SistemaDeLivraria/src/models/Status.java
@@ -1,5 +1,19 @@
 package models;
 
 public enum Status {
+	DISPONIVEL ("Disponivel"),
+	eMPRESTADO ("Emprestado"),
+	INDISPONIVEL("Indisponivel");
+	
+	private final String status;
+	
+	Status(String status){
+		this.status = status;
+	}
 
+	public String getStatus() {
+		return status;
+	}
+	
+	
 }
