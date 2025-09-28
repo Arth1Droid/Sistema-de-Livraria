8248fe0d1742a93582e99d093d131c51147c0f71
Arth1Droid <barbosarthur7@gmail.com>
2025-09-27 22:27:26 -0300

    Adiciona linhas classe Biblioteca e Autor

--- SistemaDeLivraria/src/models/Biblioteca.java
+++ SistemaDeLivraria/src/models/Biblioteca.java
@@ -1,5 +1,24 @@
 package models;
 
-public class Biblioteca {
+import java.util.List;
+import java.util.ArrayList;
 
+public class Biblioteca {
+	private List<Livro> livros = new ArrayList<>(); 
+	private List<Autor> autores = new ArrayList<>(); 
+	private List<Emprestimo> emprestimos = new ArrayList<>();
+	
+	
+	public List<Livro> getLivros() {
+		return livros;
+	}
+	public List<Autor> getAutores() {
+		return autores;
+	}
+	public List<Emprestimo> getEmprestimos() {
+		return emprestimos;
+	} 
+	
+
+	
 }
